16d592f15f65469d7f7191e83c528ffe14bf2acb
Choice E <46899181+esiokugbechoice@users.noreply.github.com>
2022-05-28 00:50:01 +0200

    Create a3pay.js

---
+++ dist/a3pay.js
@@ -0,0 +1,334 @@
+class FetchHttpHandler {
+    constructor() {
+        this.requestOptions = {
+            method: 'GET',
+            redirect: 'follow',
+            cache: "no-store"
+        };
+    }
+    send(request, callback) {
+        try {
+            fetch("https://a3pay.co/" + request, this.requestOptions)
+                .then(response => response.json())
+                .then(result => {
+                    callback(null, result);
+                })
+                .catch(error => {
+                    callback(String(error), null);
+                });
+        } catch (e) {
+            callback(e, null);
+        }
+    }
+}
+class a3_payments {
+    constructor(token) {
+        this.endpoint = 'payments/';
+        this.token = token;
+        this.httpClient = new FetchHttpHandler();
+    }
+    create_transaction(amt, lb, cur, coin, su_call, err_call, callback) {
+        if(su_call) {
+            const request = this.endpoint + 'create_transaction/?token=' + this.token + '&amount=' + amt + '&label=' + lb + '&currency=' + cur + '&coin=' + coin + '&success_callback=' + su_call + '&error_callback=' + err_call;
+        } else {
+            const request = this.endpoint + 'create_transaction/?token=' + this.token + '&amount=' + amt + '&label=' + lb + '&currency=' + cur + '&coin=' + coin;
+        }
+        this.httpClient.send(request, function(err, resp) {
+            if(resp.status == 'ok') {
+                callback(null, resp.data);
+            } else {
+                callback('Failed: ' + resp.message, null);
+            }
+        });
+    }
+    get_tx_info(txid, callback) {
+        const request = this.endpoint + 'get_tx_info/' + this.token + '/' + txid;
+        this.httpClient.send(request, function(err, resp) {
+            if(resp.status == 'ok') {
+                callback(null, resp.data);
+            } else {
+                callback('Failed: ' + resp.message, null);
+            }
+        });
+    }
+    get_tx_list(limit, callback) {
+        const request = this.endpoint + 'get_tx_list/' + this.token + '/' + limit;
+        this.httpClient.send(request, function(err, resp) {
+            if(resp.status == 'ok') {
+                callback(null, resp.data);
+            } else {
+                callback('Failed: ' + resp.message, null);
+            }
+        });
+    }
+}
+class a3_marketa {
+    constructor() {
+        this.endpoint = 'marketa/';
+        this.httpClient = new FetchHttpHandler();
+    }
+    live_data(callback) {
+        const request = this.endpoint + 'live_data';
+        this.httpClient.send(request, function(err, resp) {
+            if(resp.status == 'ok') {
+                callback(null, resp.data);
+            } else {
+                callback('Failed: ' + resp.message, null);
+            }
+        });
+    }
+    specific_data(target, symbol, option, callback) {
+        const request = this.endpoint + 'specific_data/?target=' + target + '&symbol=' + symbol + '&option=' + option;
+        this.httpClient.send(request, function(err, resp) {
+            if(resp.status == 'ok') {
+                callback(null, resp.data);
+            } else {
+                callback('Failed: ' + resp.message, null);
+            }
+        });
+    }
+    list_symbols(callback) {
+        const request = this.endpoint + 'list_symbols';
+        this.httpClient.send(request, function(err, resp) {
+            if(resp.status == 'ok') {
+                callback(null, resp.data);
+            } else {
+                callback('Failed: ' + resp.message, null);
+            }
+        });
+    }
+    list_targets(callback) {
+        const request = this.endpoint + 'list_targets';
+        this.httpClient.send(request, function(err, resp) {
+            if(resp.status == 'ok') {
+                callback(null, resp.data);
+            } else {
+                callback('Failed: ' + resp.message, null);
+            }
+        });
+    }
+}
+
+class a3_apps {
+    constructor(token) {
+        this.endpoint = 'account/';
+        this.token = token;
+        this.httpClient = new FetchHttpHandler();
+    }
+    update_password(password, newpassword, callback) {
+        const request = this.endpoint + 'update_password/?token=' + this.token + '&password=' + password + '&newPassword=' + newpassword;
+        this.httpClient.send(request, function(err, resp) {
+            if(resp.status == 'ok') {
+                callback(null, resp.data);
+            } else {
+                callback('Failed: ' + resp.message, null);
+            }
+        });
+    }
+    get_account_info(callback) {
+        const request = this.endpoint + 'get_account_info/?token=' + this.token;
+        this.httpClient.send(request, function(err, resp) {
+            if(resp.status == 'ok') {
+                callback(null, resp.data);
+            } else {
+                callback('Failed: ' + resp.message, null);
+            }
+        });
+    }
+    list_login_activity(callback) {
+        const request = this.endpoint + 'list_login_activity/?token=' + this.token;
+        this.httpClient.send(request, function(err, resp) {
+            if(resp.status == 'ok') {
+                callback(null, resp.data);
+            } else {
+                callback('Failed: ' + resp.message, null);
+            }
+        });
+
+    }
+    list_assets(callback) {
+        const request = this.endpoint + 'list_assets/?token=' + this.token;
+        this.httpClient.send(request, function(err, resp) {
+            if(resp.status == 'ok') {
+                callback(null, resp.data);
+            } else {
+                callback('Failed: ' + resp.message, null);
+            }
+        });
+
+    }
+    support_countries(callback) {
+        const request = this.endpoint + 'support_countries/?token=' + this.token;
+        this.httpClient.send(request, function(err, resp) {
+            if(resp.status == 'ok') {
+                callback(null, resp.data);
+            } else {
+                callback('Failed: ' + resp.message, null);
+            }
+        });
+
+    }
+    list_pricing(callback) {
+        const request = this.endpoint + 'list_pricing/?token=' + this.token;
+        this.httpClient.send(request, function(err, resp) {
+            if(resp.status == 'ok') {
+                callback(null, resp.data);
+            } else {
+                callback('Failed: ' + resp.message, null);
+            }
+        });
+
+    }
+    support_currencies(callback) {
+        const request = this.endpoint + 'support_currencies/?token=' + this.token;
+        this.httpClient.send(request, function(err, resp) {
+            if(resp.status == 'ok') {
+                callback(null, resp.data);
+            } else {
+                callback('Failed: ' + resp.message, null);
+            }
+        });
+
+    }
+}
+
+class a3_wallet {
+    constructor(api_key) {
+        this.api_key = api_key;
+        this.httpClient = new FetchHttpHandler();
+    }
+    get_new_address(label, callback) {
+        const request = 'get_new_address/?api_key=' + this.api_key + '&label=' + label;
+        this.httpClient.send(request, function(err, resp) {
+            if(resp.status == 'ok') {
+                callback(null, resp.data.address);
+            } else {
+                callback('Failed: ' + resp.message, null);
+            }
+        });
+    }
+    get_balance(callback) {
+        const request = 'get_balance/?api_key=' + this.api_key;
+        this.httpClient.send(request, function(err, resp) {
+            if(resp.status == 'ok') {
+                callback(null, resp.data.avaliable_balance);
+            } else {
+                callback('Failed: ' + resp.message, null);
+            }
+        });
+    }
+    get_my_addresses(limit, callback) {
+        const request = 'get_my_addresses/?api_key=' + this.api_key + '&limit=' + limit;
+        this.httpClient.send(request, function(err, resp) {
+            if(resp.status == 'ok') {
+                callback(null, resp.data.addresses);
+            } else {
+                callback('Failed: ' + resp.message, null);
+            }
+        });
+    }
+    get_address_by_label(label, callback) {
+        const request = 'get_address_by_label/?api_key=' + this.api_key + '&label=' + label;
+        this.httpClient.send(request, function(err, resp) {
+            if(resp.status == 'ok') {
+                callback(null, resp.data.address);
+            } else {
+                callback('Failed: ' + resp.message, null);
+            }
+        });
+    }
+    get_network_fee_estimate(callback) {
+        const request = 'get_network_fee_estimate/?api_key=' + this.api_key;
+        this.httpClient.send(request, function(err, resp) {
+            if(resp.status == 'ok') {
+                callback(null, resp.data.feerate);
+            } else {
+                callback('Failed: ' + resp.message, null);
+            }
+        });
+    }
+    get_transaction(txid, callback) {
+        const request = 'get_transaction/?api_key=' + this.api_key + '&txid=' + txid;
+        this.httpClient.send(request, function(err, resp) {
+            if(resp.status == 'ok') {
+                callback(null, resp.data.tx);
+            } else {
+                callback('Failed: ' + resp.message, null);
+            }
+        });
+    }
+    send_to_address(address, amount, from, callback) {
+        if(from) {
+            const request = 'send_to_address/?api_key=' + this.api_key + '&address=' + address + '&amount=' + amount + '&from=' + from;
+        } else {
+            const request = 'send_to_address/?api_key=' + this.api_key + '&address=' + address + '&amount=' + amount;
+        }
+        this.httpClient.send(request, function(err, resp) {
+            if(resp.status == 'ok') {
+                callback(null, resp.data.txId);
+            } else {
+                callback('Failed: ' + resp.message, null);
+            }
+        });
+    }
+    set_tx_fee(feerate, callback) {
+        const request = 'set_tx_fee/?api_key=' + this.api_key + '&feerate=' + feerate;
+        this.httpClient.send(request, function(err, resp) {
+            if(resp.status == 'ok') {
+                callback(null, resp.data.message);
+            } else {
+                callback('Failed: ' + resp.message, null);
+            }
+        });
+    }
+    wallet_unlock(passphrase, duration, callback) {
+        const request = 'wallet_unlock/?api_key=' + this.api_key + '&passphrase=' + passphrase + '&duration=' + duration;
+        this.httpClient.send(request, function(err, resp) {
+            if(resp.status == 'ok') {
+                callback(null, resp.data.message);
+            } else {
+                callback('Failed: ' + resp.message, null);
+            }
+        });
+    }
+    wallet_lock(callback) {
+        const request = 'wallet_lock/?api_key=' + this.api_key;
+        this.httpClient.send(request, function(err, resp) {
+            if(resp.status == 'ok') {
+                callback(null, resp.data.message);
+            } else {
+                callback('Failed: ' + resp.message, null);
+            }
+        });
+    }
+    passphrase_change(oldpassphrase, newpassphrase, callback) {
+        const request = 'passphrase_change/?api_key=' + this.api_key + '&oldpassphrase=' + oldpassphrase + '&newpassphrase=' + newpassphrase;
+        this.httpClient.send(request, function(err, resp) {
+            if(resp.status == 'ok') {
+                callback(null, resp.data.message);
+            } else {
+                callback('Failed: ' + resp.message, null);
+            }
+        });
+    }
+    dump_privkey(address, callback) {
+        const request = 'dump_privkey/?api_key=' + this.api_key + '&address=' + address;
+        this.httpClient.send(request, function(err, resp) {
+            if(resp.status == 'ok') {
+                callback(null, resp.data.privkey);
+            } else {
+                callback('Failed: ' + resp.message, null);
+            }
+        });
+    }
+    abandon_transaction(txid, callback) {
+        const request = 'abandon_transaction/?api_key=' + this.api_key + '&txid=' + txid;
+        this.httpClient.send(request, function(err, resp) {
+            if(resp.status == 'ok') {
+                callback(null, 'Transaction Canceled');
+            } else {
+                callback('Failed: ' + resp.message, null);
+            }
+        });
+    }
+}
